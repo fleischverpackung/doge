bb0cacd36afccc68a8d8615e5e02e8400ab5371f
fleischverpackung <d_o_z_e_r@gmx.net>
2017-12-21 15:05:48 +0100

    new3Dscene

--- Assets/scripts/GetHTTP.cs
+++ Assets/scripts/GetHTTP.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+
+[System.Serializable]
+class ItemArray
+{
+   // public Item[] items;
+}
+
 
 public class GetHTTP : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    //private string str = @"{"ticker":{"base":"DOGE","target":"EUR","price":"0.00706002","volume":"","change":"0.00000009"},"timestamp":1513860662,"success":true,"error":""}";
 	
-	// Update is called once per frame
+
+
+	void Start () {
+
+        ReadFile();
+
+    }
+	
+
 	void Update () {
 		
 	}
+
+    void ReadFile()
+    {
+        string text = File.ReadAllText(@"Assets/Resources/data.json");
+        Debug.Log(text);
+    }
+    
+
+    static void ReadStream()
+    {
+        string path = "Assets/Resources/data.json";
+        StreamReader reader = new StreamReader(path);
+        Debug.Log(reader.ReadToEnd());
+        reader.Close();
+    }
 }
